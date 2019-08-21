95c7f103be42179749e8cf56a25f54c404801cb5
Torsten Seemann <TSE@seas-nve.dk>
2019-08-21 18:59:45 +0200

    Added middlewares argument to TimeMachineStore ctor

--- src/Redux.DevTools/TimeMachineStore.cs
+++ src/Redux.DevTools/TimeMachineStore.cs
@@ -1,11 +1,24 @@
-﻿namespace Redux.DevTools
+﻿using System;
+using System.Linq;
+
+namespace Redux.DevTools
 {
     /// Todo : Refactor to use StoreEnhancer
     public class TimeMachineStore<TState> : Store<TimeMachineState>, IStore<TState>
     {
-        public TimeMachineStore(Reducer<TState> reducer, TState initialState = default(TState))
-            : base(new TimeMachineReducer((state, action) => reducer((TState)state, action)).Execute, new TimeMachineState(initialState))
+        public TimeMachineStore(Reducer<TState> reducer, TState initialState = default(TState), params Middleware<TState>[] middlewares)
+            : base(new TimeMachineReducer((state, action) => reducer((TState)state, action)).Execute, new TimeMachineState(initialState), WrapMiddlewares(middlewares))
         {
+        }
+
+        private static Middleware<TimeMachineState>[] WrapMiddlewares(Middleware<TState>[] middlewares)
+        {
+            return middlewares.Select(WrapMiddleware).ToArray();
+        }
+
+        private static Middleware<TimeMachineState> WrapMiddleware(Middleware<TState> middleware)
+        {
+            return store => middleware.Invoke((IStore<TState>)store);
         }
 
         TState IStore<TState>.GetState()
